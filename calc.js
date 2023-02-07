c867efe9d5c730de3c1d4e17c6ccfc7e57537fcb
utsavgohel-simform <utsav@simformsolutions.com>
2023-02-07 12:11:41 +0530

    converted functions into arrow functions.

--- calc.js
+++ calc.js
@@ -9,35 +9,36 @@ for (item of btn) {
   item.addEventListener("click", (e) => {
     btntext = e.target.innerText;
     screen.value = screen.value + btntext;
-    console.log(screen.value, btntext);
+    // console.log(screen.value, btntext);
   });
 }
-function sin() {
-  screen.value = Math.sin(screen.value);
-}
-function cos() {
-  screen.value = Math.cos(screen.value);
-}
-function tan() {
-  screen.value = Math.tan(screen.value);
-}
-function pow() {
-  screen.value = Math.pow(screen.value, 2);
-}
-function sqrt() {
-  screen.value = Math.sqrt(screen.value, 2);
-}
-function log() {
-  screen.value = Math.log10(screen.value);
-}
-function pi() {
-  screen.value = Math.PI;
-}
-function e() {
-  screen.value = Math.exp(screen.value);
-}
 
-function fact() {
+const sin = () => {
+  screen.value = Math.sin(screen.value);
+};
+const cos = () => {
+  screen.value = Math.cos(screen.value);
+};
+const tan = () => {
+  screen.value = Math.tan(screen.value);
+};
+const pow = () => {
+  screen.value = Math.pow(screen.value, 2);
+};
+const sqrt = () => {
+  screen.value = Math.sqrt(screen.value, 2);
+};
+const log = () => {
+  screen.value = Math.log10(screen.value);
+};
+const pi = () => {
+  screen.value = Math.PI;
+};
+const e = () => {
+  screen.value = Math.exp(screen.value);
+};
+
+const fact = () => {
   var i, num, f;
   f = 1;
   num = screen.value;
@@ -46,64 +47,64 @@ function fact() {
   }
   i = i - 1;
   screen.value = f;
-}
-function backspc() {
+};
+const backspc = () => {
   screen.value = screen.value.substr(0, screen.value.length - 1);
-}
+};
 let ms;
-function memory_store() {
+const memory_store = () => {
   ms = screen.value;
   screen.value = "";
   console.log(ms);
-}
+};
 
-function memory_clear() {
+const memory_clear = () => {
   ms = "";
   screen.value = "";
-}
+};
 
-function memoryPlus() {
+const memoryPlus = () => {
   screen.value = Number(screen.value) + Number(ms);
   ms = screen.value;
-}
+};
 
-function memoryMinus() {
+const memoryMinus = () => {
   screen.value = Math.abs(Number(screen.value) - Number(ms));
   ms = screen.value;
-}
+};
 
-function memory_read() {
+const memory_read = () => {
   screen.value = ms;
-}
-function syntaxError() {
+};
+const syntaxError = () => {
   if (eval(screen.value) == SyntaxError) {
     screen.value = "Syntax Error";
   }
-}
+};
 
-function onebyx() {
+const onebyx = () => {
   screen.value = 1 + "/" + screen.value;
-}
+};
 
-function ln() {
+const ln = () => {
   screen.value = Math.log(screen.value);
-}
+};
 
-function absolute() {
+const absolute = () => {
   screen.value = Math.abs(screen.value);
-}
-function plusMinus() {
+};
+const plusMinus = () => {
   if (screen.value.charAt(0) === "-") {
     screen.value = screen.value.slice(1);
   } else {
     screen.value = "-" + screen.value;
   }
-}
+};
 
-function power() {
+const power = () => {
   screen.value = Math.pow(10, screen.value);
-}
+};
 
-function random() {
+const random = () => {
   screen.value = Math.random(screen.value);
-}
+};
